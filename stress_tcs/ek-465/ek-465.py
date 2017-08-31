581162ce10c282989039fcd891f9484fa2da56e7
helimeng <helimeng@ghostcloud.cn>
2017-08-31 08:14:02 -0400

    modefied ek-465.py

--- stress_tcs/ek-465/ek-465.py
+++ stress_tcs/ek-465/ek-465.py
@@ -4,8 +4,10 @@ import ekosUtils
 from log import *
 my_utils = ekosUtils.Utils()
 
+lb_name = "stress-lb"
+app_name = "stress-app"
 stress_svcname_tmp = "stress-bootstorm-lb-"
-svc_num = 3
+svc_num = 300
 
 ip = sys.argv[1]
 testbed = sys.argv[2]
@@ -22,20 +24,19 @@ if not node_list:
 	sys.exit()
 
 
-#create 3 app
+
 def run_test():
 	#create stress_app
-	app_name = "stress_app"
 	cookies = my_utils._get_cookie(ip)
 	my_utils.create_app(ip,app_name)
 
-	info('sleep 1200 seconds')
+	info('sleep 5 seconds')
 	my_utils.bar_sleep(5)
 
 	#create stress_svc 	
 	cookies = my_utils._get_cookie(ip)
 	url = "http://" + ip + ":30000/service/stack/api/app"
-	obj_json = {"name":"stress-svc-ha-1","namespace":"default","stack":"stress_app","stateful":"none","replicas":1,"cpu":125,"memory":64,"diskSize":20000,"containers":[{"name":"hello-test-4","image":"registry.ekos.local/library/hello:latest","command":"","envs":[],"logDir":"","healthCheck":None,"cpuPercent":100,"memPercent":100,"stdin":False,"tty":False,"cfgFileMounts":[],"secretMounts":[]}],"service":{"ports":[{"protocol":"TCP","containerPort":88,"servicePort":888}]},"volumes":[],"desc":""}
+	obj_json = {"name":"stress-svc-ha-1","namespace":"default","stack":"stress-app","stateful":"none","replicas":1,"cpu":125,"memory":64,"diskSize":20000,"containers":[{"name":"hello-test-4","image":"registry.ekos.local/library/hello:latest","command":"","envs":[],"logDir":"","healthCheck":None,"cpuPercent":100,"memPercent":100,"stdin":False,"tty":False,"cfgFileMounts":[],"secretMounts":[]}],"service":{"ports":[{"protocol":"TCP","containerPort":80,"servicePort":80}]},"volumes":[],"desc":""}
 	for i in range(svc_num):
 		obj_json['name'] = stress_svcname_tmp + str(i)
 		rtn = my_utils.call_rest_api(url,"POST",cookies=cookies,json=json.dumps(obj_json))
@@ -44,61 +45,67 @@ def run_test():
 		else:
 			return False
 
-		info('sleep 1200 seconds')
-		my_utils.bar_sleep(10)
+	info('create all svc success,sleep 20 minutes')
+	my_utils.bar_sleep(1200)
 	
-	#create lb for each loadbalances
-	rtn = my_utils.create_tcp_lb_for_each_app(ip,80) 
+        #get svc name
+        svc_list = []
+        for i in range(svc_num):
+                svcname = stress_svcname_tmp + str(i)
+                svc_list.append(svcname)
+
+        #check svc status
+        print"beginning check svc status frist time"
+        rtn = my_utils.check_service_status(ip,svc_list)
+        if rtn != True:
+                sys.exit()
+
+	#create lb 
+	rtn = my_utils.create_lb(ip,lb_name) 
 	if rtn != True:
 		sys.exit()
-	info('sleep 120 seconds')
-	my_utils.bar_sleep(120)  
 
+	info('create lb success,sleep 30 seconds')
+	my_utils.bar_sleep(30)
+
+	#create_http rule for all service
+	rtn = my_utils.add_http_rule_for_all_service(ip,lb_name)
+	if rtn != True:
+		return False  
+
+	info("create http rule for all svc success,sleep 5 minutes")
+        my_utils.bar_sleep(300)
 	################################################check_app_status and check_lb_status########################################
-	#get app name
+	#get svc name
 	svc_list = []
 	for i in range(svc_num):
 		svcname = stress_svcname_tmp + str(i)
 		svc_list.append(svcname)
-	print "check_app_status first time"
 
-	#check app status
-	rtn = my_utils.check_app_status(ip,svc_list)
+	#check svc status
+	print"beginning check svc status second times"	
+	rtn = my_utils.check_service_status(ip,svc_list)
 	if rtn != True:
 		sys.exit()	
-	"""
-	rtn = my_utils.k8s_pod_health_check(ip)
-	if rtn != True:
-		return False
-	
-	#check_lb_status
-	rtn = my_utils.check_lb_status(ip)
-	if rtn != True:
-		sys.exit()
-	info('lb_status is ok!')
-	"""	
-	#let runnning 10 min
-	my_utils.bar_sleep(60)
-	print"check_app_status after runnning 10 min"	
-	#check app status
-	rtn = my_utils.check_app_status(ip,svc_list)
-	if rtn != True:
-		sys.exit()	
-	"""
-	rtn = my_utils.k8s_pod_health_check(ip)
-	if rtn != True:
-		return False
 
-	#check_lb_status
+	#check lb status
+	print "beginning check lb"
 	rtn = my_utils.check_lb_status(ip)
 	if rtn != True:
-		sys.exit()
-	info('lb_status is ok!')	
-	"""	
+		return False
+	return True
 #main
 rtn = run_test()
 if rtn == True:
 	my_utils.clean_testbed(ip)
-	info('ok')
+	info("wait for clean testbed,sleep 10 minutes")
+	my_utils.bar_sleep(600)
+	cmd = "kubectl get po | grep " + stress_svcname_tmp + "| grep Running"
+	rtn = my_utils.ssh_cmd(ip,"root","password",cmd)
+	if rtn["stdout"] == "":
+		print "clean testbed successful!"
+		info('ok')
+	else:
+		error("have any pod not delete success,run test case ek-465 failed")
 else:
 	error('run test case ek-465 failed!')
